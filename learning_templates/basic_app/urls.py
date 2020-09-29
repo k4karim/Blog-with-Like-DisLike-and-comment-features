32007bfcd8fc9a3025c4a706ba2f626f4e1560bc
k4karim <k4karim27@gmail.com>
2020-09-29 23:18:04 +0530

    Update urls.py

--- learning_templates/basic_app/urls.py
+++ learning_templates/basic_app/urls.py
@@ -6,8 +6,12 @@ app_name = 'basic_app'
 
 urlpatterns=[
     url(r'^$',views.post_list,name='post_list'),
-    
+
+    url(r'^userslist/$',views.profilepagelist,name='profilepagelist'),
+
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+
+    url(r'^userprofile/(?P<pk>\d+)/$', views.profilepage, name='profilepage'),
 
     url(r'^post/new/$',views.post_new,name='post_new'),
 
@@ -23,13 +27,20 @@ urlpatterns=[
     url(r'^post/(?P<pk>\d+)/unpost_publish/$',views.unpost_publish,name='unpost_publish'),
     url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
     url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
-    
+
     url(r'^comment/(?P<pk>\d+)/uncomment_approve/$', views.uncomment_approve, name='uncomment_approve'),
 
-    url(r'^post/(?P<pk>\d+)/upvote/$', views.upvote, name='upvote'),
+    #url(r'^post/(?P<pk>\d+)/upvote/$', views.upvote, name='upvote'),
 
-    url(r'^post/(?P<pk>\d+)/downvote/$', views.downvote, name='downvote'),
+    #url(r'^post/(?P<pk>\d+)/downvote/$', views.downvote, name='downvote'),
 
     url(r'^register/$',views.register,name='register'),
-    
+
+    url(r'^post/(?P<pk>\d+)/upvote/$',views.like,name='like'),
+
+    url(r'^post/(?P<pk>\d+)/downvote/$',views.dislike,name='dislike'),
+
+    url(r'^csv-converter/$',views.upload_csv2,name='upload_csv2'),
+
+    url(r'^asda/$',views.asda,name='asda'),
 ]
