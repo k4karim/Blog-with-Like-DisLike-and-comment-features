f0e801d8f4effd7d717f5105edae794b4999e890
k4karim <k4karim27@gmail.com>
2020-09-29 23:14:44 +0530

    Update admin.py

--- learning_templates/basic_app/admin.py
+++ learning_templates/basic_app/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post,Comment,UserProfileInfo
+from .models import Post,Comment,UserProfileInfo,Upvotes,Downvotes
 
 admin.site.register(Post)
 
 admin.site.register(Comment)
 
 admin.site.register(UserProfileInfo)
+
+admin.site.register(Upvotes)
+
+admin.site.register(Downvotes)
