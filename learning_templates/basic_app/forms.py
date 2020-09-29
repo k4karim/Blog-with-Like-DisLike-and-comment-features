7653e5f51daf76cefa65305141c8133f34437b33
k4karim <k4karim27@gmail.com>
2020-09-29 23:16:04 +0530

    Update forms.py

--- learning_templates/basic_app/forms.py
+++ learning_templates/basic_app/forms.py
@@ -9,6 +9,12 @@ class PostForm(forms.ModelForm):
         model=Post
         fields=('title','text',)
 
+        widgets={
+            'title':forms.TextInput(attrs={'class':'form-control',"placeholder": "Enter Post title",'required':True,}),
+            'text':forms.Textarea(attrs={'class':'form-control z-depth-1',"placeholder": "Write Post here",'required':True,}),
+
+            }
+
 
 class CommentForm(forms.ModelForm):
 
@@ -16,19 +22,35 @@ class CommentForm(forms.ModelForm):
         model=Comment
         fields=('text',)
 
+        widgets={
+            'text':forms.Textarea(attrs={'class':'form-control z-depth-1 input-sm',"placeholder": "write comment here"}),
+
+
+            }
+
 
 
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control',"placeholder": "Enter Your Password",'required':True,}))
 
     class Meta():
         model = User
         fields = ('username','email','password')
+        widgets={
+            'username':forms.TextInput(attrs={'class':'form-control',"placeholder": "Enter Your UserName",'required':True,}),
+            'email':forms.EmailInput(attrs={'class':'form-control',"placeholder": "Enter Your Email",'required':True,}),
 
+            }
 
 class UserProfileInfoForm(forms.ModelForm):
     class Meta():
         model = UserProfileInfo
         fields = ('profile_pic',)
+
+        widgets={
+            'profile_pic':forms.FileInput(attrs={'class':'form-control-file',"placeholder": "Enter Your UserName"}),
+
+
+            }
